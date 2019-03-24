19e52670a211be49ab2102acdcae3c3f3dd5e911
Mads <ms@madsschou.com>
2019-03-24 01:27:13 +0100

    22 - Clean up images in dummy data

--- assets/js/realEstate/data/listingsData.js
+++ assets/js/realEstate/data/listingsData.js
@@ -1,6 +1,6 @@
 var listingsData = [
   {
-    adress: 'Uetlibergstrasse 55',
+    address: 'Uetlibergstrasse 55',
     city: 'Zürich',
     rooms: 1,
     price: 220000,
@@ -10,10 +10,10 @@ var listingsData = [
       'gym'
     ],
     homeType: 'Apartment',
-    image: 'https://media.bizj.us/view/img/11038491/photo-0351*750xx2400-1350-0-225.jpg'
+    image: 'https://images.pexels.com/photos/259685/pexels-photo-259685.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
   },
   {
-    adress: 'Königstrasse 43',
+    address: 'Königstrasse 43',
     city: 'Zug',
     rooms: 2,
     price: 320000,
@@ -23,10 +23,10 @@ var listingsData = [
       'gym'
     ],
     homeType: 'Apartment',
-    image: 'https://independent.media.clients.ellingtoncms.com/img/photos/2018/10/15/202-Creekside-69-PRINT_t958_t958.jpeg?fef15e12b784e9bbb22bf3f2924819218cda3d1a'
+    image: 'https://images.pexels.com/photos/259602/pexels-photo-259602.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
   },
   {
-    adress: 'Widmerstrasser 23',
+    address: 'Widmerstrasser 23',
     city: 'Basel',
     rooms: 3,
     price: 540000,
@@ -36,10 +36,10 @@ var listingsData = [
       'gym'
     ],
     homeType: 'Apartment',
-    image: 'https://www.google.com/url?sa=i&source=imgres&cd=&cad=rja&uact=8&ved=2ahUKEwjN6taIsJnhAhWD6aQKHXfKCPQQjRx6BAgBEAU&url=https%3A%2F%2Fpatch.com%2Fillinois%2Fmanhattan%2Fs%2Fga34v%2Ffree-treehouse-no-expense-spared-mansion-illinois-wow-houses&psig=AOvVaw0wN_PY4PD9uPbS6RZW8wKO&ust=1553468671529132'
+    image: 'https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
   },
   {
-    adress: 'Uetikonstrasse 22',
+    address: 'Uetikonstrasse 22',
     city: 'Widikon',
     rooms: 4,
     price: 30000,
@@ -49,10 +49,10 @@ var listingsData = [
       'gym'
     ],
     homeType: 'Apartment',
-    image: 'https://www.google.com/url?sa=i&source=imgres&cd=&cad=rja&uact=8&ved=2ahUKEwisjeCQsJnhAhUuM-wKHZQsABQQjRx6BAgBEAU&url=http%3A%2F%2Fwww.grandviewriverhouse.com%2F1510407275%2Fpyt%2Fd0c89e2c4a4883c0%2F&psig=AOvVaw0wN_PY4PD9uPbS6RZW8wKO&ust=1553468671529132'
+    image: 'https://images.pexels.com/photos/323780/pexels-photo-323780.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
   },
   {
-    adress: 'Ringerstrasse 2',
+    address: 'Ringerstrasse 2',
     city: 'Basel',
     rooms: 5,
     price: 220000,
@@ -62,7 +62,7 @@ var listingsData = [
       'gym'
     ],
     homeType: 'Apartment',
-    image: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiL7_easJnhAhXSCuwKHUVrBNcQjRx6BAgBEAU&url=https%3A%2F%2Fairfreshener.club%2Fquotes%2Fbasketball-wallpaper-floor.html&psig=AOvVaw2XutKe0FqxPBCPY9rz-F_d&ust=1553468750800091'
+    image: 'https://images.pexels.com/photos/1030555/pexels-photo-1030555.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
   }
 ]
 
